26d2c670470339a6c9668d15ba9dafbb731827e8
Antti Saarenpää <antti.saarenpaa@gmail.com>
2020-01-07 14:15:54 +0200

    .

--- src/app.js
+++ src/app.js
@@ -3,9 +3,17 @@ import 'swiper/dist/css/swiper.css';
 import React, {useEffect, useRef, useState} from 'react';
 import ReactDOM from 'react-dom';
 import {useWindowSize} from './helpers/index.js';
+import {Tina, TinaCMS} from 'tinacms';
+import {GitClient} from '@tinacms/git-client';
 import {Swiper, Div100VhMinusYleHeader, Header} from './components/index.js';
 import {data} from './data';
 const App = () => {
+  const [cms] = useState(() => {
+    const client = new GitClient('http://localhost:3001/___tina');
+    const cms = new TinaCMS();
+    cms.registerApi('git', client);
+    return cms;
+  });
   document.body.style.margin = '0';
   const [currentIndex, updateCurrentIndex] = useState(0);
   const [nextSlideFunc, storeNextSlideFunc] = useState(() => () => null);
