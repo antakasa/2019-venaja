a4ea2bb12d714d703863e09c9ef87513cec119b4
Antti Saarenpää <antti.saarenpaa@gmail.com>
2020-06-11 14:30:23 +0300

    .

--- action/entrypoint.sh
+++ action/entrypoint.sh
@@ -11,7 +11,7 @@ FILE_EXTENSION="${FILE_NAME##*.}"
 mkdir -p "$DIR"
 if [ ! -f $OUT ]; then
 echo "Converting $IN"
-ffmpeg -i $IN -vf "select=eq(n\,0)" -q:v 3 ${OUT/$FILE_EXTENSION/$2}
+ffmpeg -i $IN -vf "select=eq(n\,0)" -q:v 3 ${OUT/$FILE_EXTENSION/$2}.jpg
 ffmpeg -i $IN -vcodec h264 -b:v 2M -maxrate 2M -bufsize 1M ${OUT/$FILE_EXTENSION/$2}
 else
 echo "Converted already."
